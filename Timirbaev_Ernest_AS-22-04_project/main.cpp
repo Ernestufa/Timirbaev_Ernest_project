c5139853e7a5f8a7b2bb1d81ff80b4b4ff277ada
Ernestufa <ernesttimirbaev@yandex.ru>
2023-09-27 02:04:03 +0500

    Created menu and added "input_pipe" and "input_cs" functions

--- Timirbaev_Ernest_AS-22-04_project/main.cpp
+++ Timirbaev_Ernest_AS-22-04_project/main.cpp
@@ -2,27 +2,31 @@
 
 using namespace std;
 
+void input_pipe();
+void input_cs();
+
 int main() {
-	setlocale(LC_ALL, "RU");
 	int number = 0;
 
-	cout << "1 Добавить трубу" << endl
-		<< "2 Добавить КС" << endl
-		<< "3 Просмотр всех объектов" << endl
-		<< "4 Редактировать трубу" << endl
-		<< "5 Редактировать КС" << endl
-		<< "6 Сохранить" << endl
-		<< "7 Загрузить" << endl
-		<< "0 Выход" << endl << endl
-		<< "Введите номер команды: ";
+	cout << "1 Add pipe" << endl
+		<< "2 Add CS" << endl
+		<< "3 View all objects" << endl
+		<< "4 Edit pipe" << endl
+		<< "5 Edit CS" << endl
+		<< "6 Save" << endl
+		<< "7 Download" << endl
+		<< "0 Exit" << endl << endl
+		<< "Enter the command number: ";
 
 	cin >> number;
 	switch (number) {
 	case 0:
 		break;
 	case 1:
+		input_pipe();
 		break;
 	case 2:
+		input_cs();
 		break;
 	case 3:
 		break;
@@ -38,4 +42,30 @@ int main() {
 		cout << "Неправильные данные, повторите попытку!";
 	}
 	return 0;
+}
+
+struct pipeline {
+	string pipe_name;
+	int pipe_length;
+	double pipe_diameter;
+	bool pipe_state;
+};
+
+struct cs {
+	string cs_name;
+	int workshops;
+	int active_workshops;
+	float effciency;
+};
+
+void input_pipe() {
+	pipeline new_pipe;
+	cin >> new_pipe.pipe_name >> new_pipe.pipe_length
+		>> new_pipe.pipe_diameter >> new_pipe.pipe_state;
+}
+
+void input_cs() {
+	cs new_cs;
+	cin >> new_cs.cs_name >> new_cs.workshops
+		>> new_cs.active_workshops >> new_cs.effciency;
 }
