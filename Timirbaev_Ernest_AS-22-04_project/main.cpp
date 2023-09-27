9d12e55662dff223282938c23ef311d4c3265d11
Ernestufa <ernesttimirbaev@yandex.ru>
2023-09-28 03:41:54 +0500

    Учёл возможность ошибки пользователя при вводе

--- Timirbaev_Ernest_AS-22-04_project/main.cpp
+++ Timirbaev_Ernest_AS-22-04_project/main.cpp
@@ -8,38 +8,49 @@ void input_cs();
 int main() {
 	int number = 0;
 
-	cout << "1 Add pipe" << endl
-		<< "2 Add CS" << endl
-		<< "3 View all objects" << endl
-		<< "4 Edit pipe" << endl
-		<< "5 Edit CS" << endl
-		<< "6 Save" << endl
-		<< "7 Download" << endl
-		<< "0 Exit" << endl << endl
-		<< "Enter the command number: ";
+	while (true) {
 
-	cin >> number;
-	switch (number) {
-	case 0:
-		break;
-	case 1:
-		input_pipe();
-		break;
-	case 2:
-		input_cs();
-		break;
-	case 3:
-		break;
-	case 4:
-		break;
-	case 5:
-		break;
-	case 6:
-		break;
-	case 7:
-		break;
-	default:
-		cout << "Ќеправильные данные, повторите попытку!";
+		cout << "1 Add pipe" << endl
+			<< "2 Add CS" << endl
+			<< "3 View all objects" << endl
+			<< "4 Edit pipe" << endl
+			<< "5 Edit CS" << endl
+			<< "6 Save" << endl
+			<< "7 Download" << endl
+			<< "0 Exit" << endl << endl
+			<< "Enter the command number: ";
+
+		cin >> number;
+		if (cin.fail()) {
+			cout << "Error! Try again" << endl << endl;
+			cin.clear();
+		} else {
+			switch (number) {
+			case 0:
+				exit(0);
+			case 1:
+				input_pipe();
+				break;
+			case 2:
+				input_cs();
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			default:
+				cout << "Error! Try again" << endl << endl;
+			}
+		}
+
+		cin.clear();
+		cin.ignore(1000, '\n');
 	}
 	return 0;
 }
@@ -60,12 +71,16 @@ struct cs {
 
 void input_pipe() {
 	pipeline new_pipe;
-	cin >> new_pipe.pipe_name >> new_pipe.pipe_length
-		>> new_pipe.pipe_diameter >> new_pipe.pipe_state;
+	cout << "Enter pipe name: "; cin >> new_pipe.pipe_name;
+	cout << "Enter pipe length: ";  cin >> new_pipe.pipe_length;
+	cout << "Enter pipe diameter: "; cin >> new_pipe.pipe_diameter;
+	cout << "Enter state in repair: "; cin >> new_pipe.pipe_state; cout << endl;
 }
 
 void input_cs() {
 	cs new_cs;
-	cin >> new_cs.cs_name >> new_cs.workshops
-		>> new_cs.active_workshops >> new_cs.effciency;
+	cout << "Enter cs name: "; cin >> new_cs.cs_name;
+	cout << "Enter number of workshops: "; cin >> new_cs.workshops;
+	cout << "Enter number of active workshops: "; cin >> new_cs.active_workshops;
+	cout << "Enter effciency: "; cin >> new_cs.effciency; cout << endl;
 }
